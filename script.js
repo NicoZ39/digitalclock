f3110e558e6054eb00ad8800ccca2d556de92e3f
NicoZ39 <nicolaslogerot@icloud.com>
2024-09-14 14:47:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ function updateClock() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    
-    // Date
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const fullDate = now.toLocaleDateString('fr-FR', options);
-    const [day, date] = fullDate.split(", ");
-    
-    // Mise à jour de l'affichage
+
     document.getElementById('hours').textContent = hours;
     document.getElementById('minutes').textContent = minutes;
     document.getElementById('seconds').textContent = seconds;
-    
-    document.getElementById('day').textContent = day;
-    document.getElementById('fullDate').textContent = date;
+
+    // Date
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const dayOfWeek = now.toLocaleDateString('fr-FR', { weekday: 'long' });
+    const fullDate = now.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' });
+
+    document.getElementById('dayOfWeek').textContent = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1); // Majuscule au début
+    document.getElementById('fullDate').textContent = fullDate;
 }
 
 setInterval(updateClock, 1000);
